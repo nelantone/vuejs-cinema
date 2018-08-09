5d28cc2a7781d18af447619f991ed7f4b89de5fd
Antonio Serna <antonelo@gmail.com>
2018-08-09 12:12:50 +0200

    L70 add filter genres items

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres'; 
 
 new Vue({
   el: '#app',
@@ -19,9 +20,22 @@ new Vue({
       }
     },
     'movie-filter': {
+      data() {
+        return {
+          genres
+        }
+      },
       template: `<div id="movie-filter">
                     <h2>Filter Results</h2>
-                 </div>`
+                    <div class="filter-group">
+                      <check-filter v-for="genre in genres"></check-filter>
+                    </div>
+                  </div>`,
+      components: {
+        'check-filter': {
+          template: `<div>Filter</div>`
+        }
+      }
     }
   }
 });
