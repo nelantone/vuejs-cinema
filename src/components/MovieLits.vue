36249c7bb12d90ebd1c102c1708cf26d2175afe2
Antonio Serna <antonelo@gmail.com>
2018-08-10 10:43:34 +0200

    L85 remplace dummy data with the API and refactor genres *Tom

--- src/components/MovieLits.vue
+++ src/components/MovieLits.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="movie-list">
-    <div v-for="movie in filteredMovies" class="movie" v-bind:key="movie">{{ movie.title }}</div>
+    <div v-for="movie in filteredMovies" class="movie" v-bind:key="movie">{{ movie.movie.Title }}</div>
   </div>
 </template>
 <script>
   import genres from '../util/genres'; 
   export default {
-    data() {
-      return {
-        movies: [
-          { title: "pulp fiction", genre: genres.CRIME },
-          { title: "home alone", genre: genres.COMEDY },
-          { title: "Austin Powers", genre: genres.COMEDY }
-        ]
-      }
-    },
-    props: ['genres', 'time' ],
+    props: ['genres', 'time', 'movies' ],
     methods: {
-      moviePassesGenreFilter(movie) {
-          return this.genres.find(genre => movie.genre === genre);
+      moviePassesGenreFilter(item) {
+          let movieGenres = item.movie.Genre.split(', ')
+          return this.genres.find(genre => movieGenres.includes(genre));
         // find((genre)=>{
         //   return movie.genre === genre
         // })
