83bccde4773904d5b752ab281212d38c3719f60b
Antonio Serna <antonelo@gmail.com>
2018-08-11 14:31:36 +0200

    L93 Adding time filters

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="movie-filter">
     <h2>Filter Results</h2>
+    <h3>By Time of the day</h3>
     <div class="filter-group">
-      <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter" v-bind:key="genre"></check-filter>
+      <check-filter v-for="time in times" category="time" v-bind:title="time" v-on:check-filter="checkFilter" v-bind:key="time"></check-filter>
+    </div>
+    <h3>By Genre</h3>
+    <div class="filter-group">
+      <check-filter v-for="genre in genres" category="genre" v-bind:title="genre" v-on:check-filter="checkFilter" v-bind:key="genre"></check-filter>
     </div>
   </div>
 </template>
 <script>
   import genres from '../util/genres';
+  import times from '../util/times';
   import CheckFilter from './CheckFilter.vue';
   
   export default {
     data() {
       return {
-        genres
+        genres,
+        times
       }
     },
     methods: {
