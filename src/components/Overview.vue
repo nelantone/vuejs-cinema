2518271282936bb41ff97eac9b71bd1a0c6ce776
Antonio Serna <antonelo@gmail.com>
2018-08-13 13:17:54 +0200

    L100 Abstract main page into overview component

---
+++ src/components/Overview.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="overview">
+        <div class="main">
+            <movie-list v-bind:genres="genres" v-bind:time="time" v-bind:movies="movies" v-bind:day="day"></movie-list>
+            <movie-filter></movie-filter>
+        </div>
+    </div>
+</template>
+<script>
+import MovieList from './MovieLits.vue';
+import MovieFilter from './MovieFilter.vue';
+
+  export default {
+    props: ['genres', 'time', 'movies', 'day' ],
+    components: {
+      MovieList,
+      MovieFilter
+    },
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import './style.scss';
 
-import MovieList from './components/MovieLits.vue';
-import MovieFilter from './components/MovieFilter.vue';
+import Overview from './components/Overview.vue';
 
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
@@ -27,8 +26,7 @@ new Vue({
     bus
   },
   components: {
-    MovieList,
-    MovieFilter
+    Overview
   },
   created () {
     this.$http.get('/api').then( response => { 
